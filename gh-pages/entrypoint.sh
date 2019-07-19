0864bd9d5f44504d2902d965d395abee4638525b
Mark Ayers <mark@philoserf.com>
2019-07-19 16:01:19 -0700

    Use the correct remote (#28)

--- gh-pages/entrypoint.sh
+++ gh-pages/entrypoint.sh
@@ -11,4 +11,4 @@ git config user.name "${GITHUB_ACTOR}"
 git config user.email "${GITHUB_ACTOR}@users.noreply.github.com"
 git add .
 git commit -m "build gh-pages at $(date +%Y%m%dT%H%M)"
-git push --force "https://${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git" master:gh-pages
+git push --force "https://${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git" gh-pages
