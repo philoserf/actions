0afeb1ab518cca54b9363b3a07670ed318d8b603
Mark Ayers <mark@philoserf.com>
2019-07-19 14:50:25 -0700

    Add gh-pages actions (#21)

---
+++ gh-pages/entrypoint.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+error() {
+	echo "$1"
+	exit 1
+}
+
+bundle install
+bundle exec jekyll build
+cd build
+git init
+git config user.name "${GITHUB_ACTOR}"
+git config user.email "${GITHUB_ACTOR}@users.noreply.github.com"
+git add .
+git commit -m "build gh-pages at $(date +%Y%m%dT%H%M)"
+git push --force "https://${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git" master:gh-pages
