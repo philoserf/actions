c5417e9e58316c08a66df829378cf467b85ae66a
Mark Ayers <mark@philoserf.com>
2019-07-19 16:10:40 -0700

    Hack hack

--- gh-pages/entrypoint.sh
+++ gh-pages/entrypoint.sh
@@ -3,6 +3,20 @@
 set -o errexit
 set -o nounset
 
+remote_repo="https://${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
+
+echo ${GITHUB_REPOSITORY} | grep -E '^([a-z]*)\/\1\.github\.io$' >/dev/null
+if [ $? -eq 0 ]; then
+	remote_branch="master"
+else
+	remote_branch="gh-pages"
+fi
+
+if [ "${GITHUB_REF}" == "refs/heads/${remote_branch}" ]; then
+	echo "Cannot publish on branch ${remote_branch}"
+	exit 1
+fi
+
 bundle install
 bundle exec jekyll build
 cd build
@@ -10,5 +24,5 @@ git init
 git config user.name "${GITHUB_ACTOR}"
 git config user.email "${GITHUB_ACTOR}@users.noreply.github.com"
 git add .
-git commit -m "build gh-pages at $(date +%Y%m%dT%H%M)"
-git push --force "https://${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git" gh-pages
+git commit -m "jekyll build at $(date +%Y%m%dT%H%M)"
+git push --force $remote_repo master:$remote_branch
