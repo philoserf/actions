541cf79ad04781676638bf22a4b0a69a4875b046
Mark Ayers <mark@philoserf.com>
2020-02-12 15:49:49 -0800

    Update shfmt in base

--- base/entrypoint.sh
+++ base/entrypoint.sh
@@ -1,12 +1,12 @@
-#!/bin/sh
+#!/bin/bash
 
 set -o errexit
 set -o nounset
 set -o pipefail
 
 error() {
-	echo "$1"
-	exit 1
+  echo "$1"
+  exit 1
 }
 
 echo "This is a base image for GitHub Actions and other useful work."
